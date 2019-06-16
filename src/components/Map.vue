c98b2d44f46e9d2be079a601a78212ccb6d47a1d
Donovan Risk <donnierisk@gmail.com>
2019-06-16 14:20:51 +0200

    Crime map using Google maps API

---
+++ src/components/Map.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<h3>{{ mapName }}:</h3>
+		<div id="mapContainer">
+			<GmapMap
+				:center="{lat:-33.918861, lng:18.423300}"
+				:zoom="12"
+				map-type-id="terrain"
+				style="width: 100%; height: 100%"
+			>
+			</GmapMap>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+	data: function () {
+		return {
+			mapName: "Crime Map",
+		}
+	},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#mapContainer {
+  width: 800px;
+  height: 600px;
+  margin: 0 auto;
+  background: gray;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDnkCpz57JyEC6kQ4rqIJglbsy_1mJXzc0',
+  },
+ })
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
